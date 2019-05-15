dad3a3bf88e8b1194340b3a4af3ccb3749eeb061
Otávio Baziewicz Filho <otavio.baziewicz.filho@gmail.com>
2019-05-15 15:59:21 -0300

    Initial Commit

---
+++ Windows98Crash/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Windows98Crash
+//
+//  Created by Otávio Baziewicz Filho on 15/05/19.
+//  Copyright © 2019 Otávio Baziewicz Filho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
